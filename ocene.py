873c3a6ef9f019df26a635baeb12e1d4ab13494a
Mila Kostic <milakostic.eng@gmail.com>
2023-02-05 16:51:01 +0100

    ocene

---
+++ ocene.py
@@ -0,0 +1,9 @@
+Zikine_ocene = [2, 4, 5, 3, 5]
+
+najmanja = min(Zikine_ocene) # поправи овај ред
+najveca = max(Zikine_ocene) # поправи овај ред
+broj = len(Zikine_ocene) # поправи овај ред
+print("Zikine ocene su:", Zikine_ocene)
+print("Zikina najmanja ocena iz informatike je:", najmanja)
+print("Zikina najveca ocena iz informatike je:", najveca)
+print("Zikina broj ocena iz informatike je:", broj)
